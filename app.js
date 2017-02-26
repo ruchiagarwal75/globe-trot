2e41b0dc68739b3140b0338a96b55481ef2dbc38
ruchiagarwal75 <ruchiii75@gmail.com>
2017-02-25 18:59:52 -0800

    added login and dashboard

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
 var express = require('express');
 var app = express();
-
+var MongoClient = require('mongodb').MongoClient;
+var bodyParser = require('body-parser');
+var dbCOnnectionObj;
+// Connect to the db
+MongoClient.connect("mongodb://admin:password@ds145359.mlab.com:45359/globe_trot", function(err, db) {
+  if(!err) {
+    console.log("Database connection made!");
+    dbCOnnectionObj = db;
+  }
+  else {
+      console.log('Error in connecting to database');
+  }
+});
+//adding static files
 app.use('/static', express.static('static'));
+//adding body parser to fetch post requests
+var bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+app.post('/login', function (req, res) {
+    var collection = dbCOnnectionObj.collection('users');
+    collection.findOne({Email: req.body.email}, function(err, item) {
+        if (item) {
+            res.sendFile(__dirname + '/dashboard.html');
+        }
+        else {
+            console.log('Bad req');
+        }
+    });
+});
+app.get('/addtrip', function (req, res) {
+
+});
 app.listen('8000', 'localhost');
 console.log('Server started at 8000');
