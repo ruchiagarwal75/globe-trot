151a4744f1c7986bf8de4f60be07547fb4e2aeea
ruchiagarwal75 <ruchiii75@gmail.com>
2017-03-02 19:12:33 -0800

    added mytrips page

--- app.js
+++ app.js
@@ -15,8 +15,12 @@ MongoClient.connect("mongodb://admin:password@ds145359.mlab.com:45359/globe_trot
 });
 //adding static files
 app.use('/static', express.static('static'));
+
 //adding body parser to fetch post requests
 var bodyParser=require('body-parser');
+
+//setting view engine
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function (req, res) {
@@ -27,7 +31,8 @@ app.post('/login', function (req, res) {
     var collection = dbCOnnectionObj.collection('users');
     collection.findOne({Email: req.body.email}, function(err, item) {
         if (item) {
-            res.sendFile(__dirname + '/dashboard.html');
+            res.cookie('user', req.body.email);
+            res.redirect('/dashboard');
         }
         else {
             console.log('Bad req');
@@ -36,20 +41,36 @@ app.post('/login', function (req, res) {
 });
 app.post('/addtrip', function (req, res) {
     var collection = dbCOnnectionObj.collection('TravelDetails');
-    collection.insertOne({StartPoint:req.body.origincity,EndPoint:req.body.destinationcity,StartDate:req.body.startdate}, function(err, item) {
+    //console.log(JSON.stringify(req.body));
+    collection.insertOne({user:'r@gmail.com',StartPoint:req.body.origincity,EndPoint:req.body.destinationcity,StartDate:req.body.startdate}, function(err, item) {
         if (item) {
-            res.sendFile(__dirname + '/mytrips.html');
+            res.redirect('/mytrips');
         }
         else {
             console.log('Bad req');
         }
-    });
+   });
 });
 app.get('/dashboard', function (req, res) {
+    console.log(req.cookies);
     res.sendFile(__dirname + '/dashboard.html');
 });
 app.get('/addtrip', function (req, res) {
      res.sendFile(__dirname + '/addtrip.html');
 });
-app.listen('8000', 'localhost');
-console.log('Server started at 8000');
+app.get('/mytrips', function (req, res) {
+    var userTrips = [];
+    var collection = dbCOnnectionObj.collection('TravelDetails');
+    collection.find({user: 'r@gmail.com'}, function(err, trips) {
+        trips.each(function(err, item){
+         if(item)
+            userTrips.push(item);
+        });
+    });
+    setTimeout(function(){
+        res.render('mytrips', {trips: userTrips});  
+        res.end(); 
+    },1000);
+});
+app.listen('8001', 'localhost');
+console.log('Server started at 8001');
